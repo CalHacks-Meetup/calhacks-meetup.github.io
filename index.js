17a04a1d8ffdb1f04726d6ee2ce79464db8bc47b
Garrett Tan <garrett.t.tan@gmail.com>
2015-10-10 15:50:46 -0700

    Fixed javascript origin issue

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 /* 	Requiring all middleware that app is using
 	will require the API's we're using here. */
 var express = require('express');
+var oauth = require('oauth');
+var mongo = require('mongodb');
+var gcal = require('google-calendar');
+var q = require('q');
 
 // Create an express app
+var oa;
 var app = express();
+var clientId = 'GOOGLE_CLIENT_ID';
+var clientSecret = 'GOOGLE_CLIENT_SECRET';
+var scopes = 'https://www.googleapis.com/auth/calendar';
+var googleUserId;
+var refreshToken;
+var baseUrl;
 
-app.set('port', (process.env.PORT || 5000));
+
+app.set('port', (process.env.PORT || 8000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -36,3 +48,56 @@ app.listen(app.get('port'), function() {
 });
 
 
+// DATABASE SET UP 
+var mongoCollectionName = 'MONGO_COLLECTION_NAME';
+var mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost/default';
+
+function connect(callback) {
+	var deferred = q.defer();
+	if (database == undefined) {
+		mongo.Db.connect(mongoUri, function (err, db) {
+			if (err) deferred.reject({error: err});
+			database = db;
+			deferred.resolve();
+		});
+	} else {
+		deferred.resolve();
+	}
+	return deferred.promise;
+}
+
+// GOOGLE AUTHORIZATION
+function authorize() {
+	var deferred = q.defer();
+	oa = new oauth.OAuth2(clientId, 
+		clientSecret, "https://accounts.google.com/o", 
+		"/oauth2/auth", "/oauth2/token");
+	if (refreshToken) {
+		oa.getOAuthAccessToken(refreshToken, 
+			{grant_type: 'refresh_token', clientId: clientId, 
+			client_secret: clientSecret}, 
+			function(err, access_token, refresh_token, res) {
+				// Lookup settings from database
+				connect().then(function() {
+					database.collection(mongoCollectionName).findOne(
+						{google_user_id: googleUserId}, 
+						function(findError, settings) {
+							var expiresIn = parseInt(res.expires_in);
+							var accessTokenExpiration = new Date().getTime() + (expiresIn*1000);
+							// Add refresh token if it is returned
+							if (refresh_token != undefined) {
+								settings.google_refresh_token = refresh_token;
+							}
+							// Update access token in database
+							settings.google_access_token = access_token;
+							settings.google_access_token_expiration = accessTokenExpiration;
+							database.collection(mongoCollectionName).save(settings);
+							deferred.resolve(access_token);
+						});
+				});
+			})
+	} else {
+		deferred.reject({error: 'Application needs authorization.'});
+	}
+	return deferred.promise;
+}
